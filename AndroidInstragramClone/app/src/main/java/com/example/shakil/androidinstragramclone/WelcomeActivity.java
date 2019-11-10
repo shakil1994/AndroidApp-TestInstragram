acb448386ff8cb82d8a3983cc7b1cfa73fd47522
android_shakil <shakhawathossen53@gmail.com>
2019-11-11 00:42:40 +0600

    Added BottomNavigationView

--- AndroidInstragramClone/app/src/main/java/com/example/shakil/androidinstragramclone/WelcomeActivity.java
+++ AndroidInstragramClone/app/src/main/java/com/example/shakil/androidinstragramclone/WelcomeActivity.java
@@ -1,14 +1,69 @@
 package com.example.shakil.androidinstragramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import com.example.shakil.androidinstragramclone.Fragments.HomeFragment;
+import com.example.shakil.androidinstragramclone.Fragments.NotificationFragment;
+import com.example.shakil.androidinstragramclone.Fragments.ProfileFragment;
+import com.example.shakil.androidinstragramclone.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    Fragment selectedFragment = null;
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottom_navigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ButterKnife.bind(this);
+
+        bottom_navigation.setOnNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.nav_search:
+                    selectedFragment = new SearchFragment();
+                    break;
+
+                case R.id.nav_add:
+                    selectedFragment = null;
+                    startActivity(new Intent(WelcomeActivity.this, PostActivity.class));
+                    break;
+
+                case R.id.nav_favorite:
+                    selectedFragment = new NotificationFragment();
+                    break;
+
+                case R.id.nav_profile:
+                    SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                    editor.putString("PROFILEID", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    editor.apply();
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+            if (selectedFragment != null){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return true;
+        });
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
     }
 }
